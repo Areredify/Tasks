6ab2db7a2571dac0d25e1081b7579c4cd80a8f84
Areredify <misha-babenko@yandex.ru>
2018-10-15 08:55:19 +0300

    kostil

--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -13,9 +13,7 @@
 
 int main(int argc, char **argv)
 {
-    //gpu::Device device = gpu::chooseGPUDevice(argc, argv);
-    char *argvv[] = { "poop", "1" };
-    gpu::Device device = gpu::chooseGPUDevice(2, argvv);
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
     gpu::Context context;
     context.init(device.device_id_opencl);
